ba6208478e7e2a13f8a7338779b41bdbd46ee0d7
Saheed Lamina git config --global user.email you@example.comgit config --global user.name Saheed <agbolamina@hotmail.com>
2020-07-01 09:04:10 +0100

    variables

--- variables.py
+++ variables.py
@@ -1,14 +1,14 @@
 
-print("hello world")
+# print("hello world")
 
 #Let's create a variable
 
 # AGE= 10 #int
 # SHOE SIZE= 3.6 #float
-name = input("please enter your name") #string
-age = input("please enter your age")
-print(name)
-print(age)
+# name = input("please enter your name") #string
+# age = input("please enter your age")
+# print(name)
+# print(age)
 
 # print(x + y)
 #
@@ -23,15 +23,14 @@ print(age)
 # #above code is called casting, turning a string into an int
 
 # create a variable called first_name and last_name
+first_name = input("What is your first name?  ")
+last_name = input("What is your last name?  ")
 
+full_name = first_name + " " + last_name
 
-# create a variable called full_name and display full_name
+age = input("Thank you!" + full_name + ". How old are you?  ")
+address = input("\nAnd what is your address?  ")
 
-# create a variable called age
-
-# create a variable called address
-
-# prompt the user to get all the above info
-
+print("So, to conclude " + full_name + ". You are " + age + " years old and live at " + address + "...")
 # display all
 
